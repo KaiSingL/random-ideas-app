8e892ec0d5c5d933d91cf8946ea90a6a41b4a0f2
KaiSingL <laut9810@gmail.com>
2023-10-02 20:32:24 +0800

    fixed the upload date format in the idea card

--- client/src/components/IdeaList.js
+++ client/src/components/IdeaList.js
@@ -76,7 +76,7 @@ class IdeaList {
       </h3>
       <p class="tag ${tagClass}">${idea.tag.toUpperCase()}</p>
       <p>
-        Posted on <span class="date">${idea.date}</span> by
+        Posted on <span class="date">${formatDate(idea.date)}</span> by
         <span class="author">${idea.username}</span>
       </p>
     </div>`;
@@ -84,6 +84,22 @@ class IdeaList {
 			.join('');
 		this.addEventListeners();
 	}
+}
+
+function formatDate(inputDate) {
+	const date = new Date(inputDate);
+
+	const year = date.getFullYear();
+	const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-indexed in JavaScript
+	const day = date.getDate().toString().padStart(2, '0');
+
+	const hours = date.getHours().toString().padStart(2, '0');
+	const minutes = date.getMinutes().toString().padStart(2, '0');
+	const seconds = date.getSeconds().toString().padStart(2, '0');
+
+	const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+	return formattedDate;
 }
 
 export default IdeaList;
